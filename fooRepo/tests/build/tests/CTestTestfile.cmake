b05867b90a1c5fab8c1e4006434dc2ab2a667f36
Carlos Garzon <showlove@knights.ucf.edu>
2020-04-08 19:16:18 -0400

    first commit

--- fooRepo/tests/build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/carlosgarzon/Desktop/code/cpp/codeQs/tests
-# Build directory: /Users/carlosgarzon/Desktop/code/cpp/codeQs/tests/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gtestProject "gtestProject")
